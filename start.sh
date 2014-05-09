28a55698f44d595c6b97a764f9ecaf3a9753ddbc
Daniel Kurka <dankurka@google.com>
2014-05-09 16:22:58 +0200

    Add launcher.
    Change-Id: Ie26632c2880271ff00fa65de28cba0beebf80b24

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+echo "Starting build"
+mvn -q install > /dev/null
+pushd launcher
+trap '{ echo "Hey, you pressed Ctrl-C.  Time to quit." ; popd; exit 0; }' INT
+echo "Starting server"
+mvn -q exec:java -Dexec.mainClass=com.google.gwt.benchmark.launcher.Launcher \
+    -DconfigFile=../compileserver/config/config
+
