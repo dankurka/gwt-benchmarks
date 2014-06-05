f11421af7859e4a4de9be606e5bc838a9af21fdb
Daniel Kurka <dankurka@google.com>
2014-06-05 16:52:30 +0200

    Store compile server config outside of the repo.
    Change-Id: Ic36895430bffb38d47b989859462e137089cceff

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ pushd launcher
 trap '{ echo "Hey, you pressed Ctrl-C.  Time to quit." ; popd; exit 0; }' INT
 echo "Starting server"
 mvn -q exec:java -Dexec.mainClass=com.google.gwt.benchmark.launcher.Launcher \
-    -DconfigFile=../compileserver/config/config
+    -DconfigFile=../../compileserver_config
 
