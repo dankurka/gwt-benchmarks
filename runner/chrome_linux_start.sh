d93fc785c83b06f00147d7a6b5c8d2c4a9f74913
Daniel Kurka <dankurka@google.com>
2014-06-05 16:47:53 +0200

    Add chrome web driver.
    Change-Id: Ibe748c5910b414ddbbe61bdc5576425fb96782f4

--- runner/chrome_linux_start.sh
+++ runner/chrome_linux_start.sh
@@ -1,3 +1,4 @@
 java -jar selenium-server-standalone-2.40.0.jar -role node \
   -hub http://172.28.82.238:4444/grid/register \
-  -forcedBrowserMode chrome -maxSession 1
+  -forcedBrowserMode chrome -maxSession 1 \
+  -Dwebdriver.chrome.driver=chromedriver_linux_x64_2.10
