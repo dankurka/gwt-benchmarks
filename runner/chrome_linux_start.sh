2ccab15d9e68679f382856590f02e436c7a65376
Daniel Kurka <dankurka@google.com>
2015-07-08 11:22:42 +0200

    Update selenium to 2.46
    Change-Id: Ib46d7b8e9c8bdc1e7bf7847793c6d951659a50f1

--- runner/chrome_linux_start.sh
+++ runner/chrome_linux_start.sh
@@ -1,5 +1,5 @@
-java -jar selenium-server-standalone-2.40.0.jar -role node \
+java -jar selenium-server-standalone-2.46.0.jar -role node \
   -hub http://172.28.82.238:4444/grid/register \
   -forcedBrowserMode chrome -maxSession 1 \
-  -Dwebdriver.chrome.driver=chromedriver_linux_x64_2.10 \
+  -Dwebdriver.chrome.driver=chromedriver_linux_x64_2.16 \
   -browser browserName=chrome,maxInstances=1,platform=LINUX
--- runner/firefox_linux_start.sh
+++ runner/firefox_linux_start.sh
@@ -1,4 +1,4 @@
-java -jar selenium-server-standalone-2.40.0.jar -role node \
+java -jar selenium-server-standalone-2.46.0.jar -role node \
   -hub http://172.28.82.238:4444/grid/register \
   -forcedBrowserMode chrome -maxSession 1 \
   -browser browserName=firefox,maxInstances=1,platform=LINUX
