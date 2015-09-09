a7d9dcb619d347d7716717ae574e252b80bc6535
Goktug Gokdogan <goktug@google.com>
2015-09-08 21:15:13 -0700

    Adds benchmarks for ArrayList
    Change-Id: I5c1fcd2ac65fdd88552dcd9f35a8afe7867e3a46

---
+++ benchmarks/src/main/java/com/google/gwt/benchmark/benchmarks/java/util/shared/ArrayListObjectAddBenchmark.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.benchmark.benchmarks.java.util.shared;
+
+import com.google.gwt.benchmark.framework.client.AbstractBenchmarkEntryPoint;
+import com.google.gwt.benchmark.framework.shared.AbstractBenchmark;
+
+import java.util.ArrayList;
+
+/**
+ * Benchmark array set performance.
+ */
+public class ArrayListObjectAddBenchmark extends AbstractBenchmark {
+
+  public static class EntryPoint extends AbstractBenchmarkEntryPoint {
+    @Override
+    protected AbstractBenchmark getBenchmark() {
+      return new ArrayListObjectAddBenchmark();
+    }
+  }
+
+  private ArrayList<Object> arrayList;
+  private Object testObjectInstance = "test";
+
+  public ArrayListObjectAddBenchmark() {
+    super("ArrayTestObjectGetBenchmark");
+  }
+
+  @Override
+  public Object run() {
+    for (int i = 0; i < 1000; i++) {
+      arrayList.add(testObjectInstance);
+    }
+
+    return arrayList;
+  }
+
+  @Override
+  public void setup() {
+    arrayList = new ArrayList<Object>();
+  }
+}
