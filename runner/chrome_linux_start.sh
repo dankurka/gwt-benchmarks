2d5705ae938b271582eff06373b95df4bf6e7755
Daniel Kurka <dankurka@google.com>
2016-03-15 14:45:11 +0100

    Update chrome driver

--- runner/chrome_linux_start.sh
+++ runner/chrome_linux_start.sh
@@ -1,5 +1,5 @@
 java -jar selenium-server-standalone-2.46.0.jar -role node \
   -hub http://100.107.70.41:4444/grid/register \
   -forcedBrowserMode chrome -maxSession 1 \
-  -Dwebdriver.chrome.driver=chromedriver_linux_x64_2.16 \
+  -Dwebdriver.chrome.driver=chromedriver_linux_x64_2.21 \
   -browser browserName=chrome,maxInstances=1,platform=LINUX
