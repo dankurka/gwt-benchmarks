da24456e9a8189dd019a46e1af647298d5b6dca4
Daniel Kurka <dankurka@google.com>
2016-03-15 14:06:19 +0100

    Update ips
    Change-Id: Ia42a4b29421502cf45b8de630206b1a613d72f78

--- runner/ie11_windows_start.bat
+++ runner/ie11_windows_start.bat
@@ -1,5 +1,5 @@
 java -jar selenium-server-standalone-2.46.0.jar -role node ^
-  -hub http://172.28.82.238:4444/grid/register ^
+  -hub http://100.107.70.41:4444/grid/register ^
   -forcedBrowserMode chrome -maxSession 1 ^
   -browser browserName="internet explorer,version=11,maxInstances=1,platform=WINDOWS" ^
   -Dwebdriver.ie.driver=IEDriverServer_x64_2.46.0.exe
